2a925919653e2524fd854c807fb8d9b0c1f9c16d
Newton Miranda <newton.chickengopro@gmail.com>
2022-02-03 10:02:03 -0300

    Add Cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import routes from '@routes/index';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
